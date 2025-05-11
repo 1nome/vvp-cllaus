0b676b508a6edc5b71709eff86f971057e65f268
1nome <stashfoos@gmail.com>
2025-05-11 02:24:53 +0200

    zoom, respond only ro left click, more keybings

--- tests.py
+++ tests.py
@@ -52,22 +52,37 @@ while running:
     base_offset = ((-x_offset if x_offset < 0 else 0) // size, (-y_offset if y_offset < 0 else 0) // size)
     visible_dims = ((screen_dims[0] // size + 1), (screen_dims[1] // size + 1))
 
+    # zooms in the opposite direction for some reason
+    def zoom(dir, x_pos, y_pos):
+        newsize = size + (1 if dir else (-1 if size > 1 else 0))
+        x_rel = x_pos - screen_dims[0]
+        y_rel = y_pos - screen_dims[1]
+        x_cur = x_offset + x_pos - screen_dims[0]
+        y_cur = y_offset + y_pos - screen_dims[1]
+        return (((x_offset + x_rel) // size) * newsize - x_rel, ((y_offset + y_rel) // size) * newsize - y_rel, newsize)
+
     # event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             # stops the loop (thus pygame)
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_down = True
+            if event.button == pygame.BUTTON_LEFT:
+                mouse_down = True
+            if event.button == pygame.BUTTON_WHEELDOWN:
+                x_offset, y_offset, size = zoom(False, event.pos[0], event.pos[1])
+            if event.button == pygame.BUTTON_WHEELUP:
+                x_offset, y_offset, size = zoom(True, event.pos[0], event.pos[1])
         if event.type == pygame.MOUSEBUTTONUP:
-            if mouse_down and not mouse_move:
-                # toggle cell under cursor
-                x = (-x_offset + event.pos[0]) // size
-                y = (-y_offset + event.pos[1]) // size
-                if 0 < x < universe.shape[0] and 0 < y < universe.shape[1]:
-                    universe[x, y] = not universe[x, y]
-            mouse_down = False
-            mouse_move = False
+            if event.button == pygame.BUTTON_LEFT:
+                if mouse_down and not mouse_move:
+                    # toggle cell under cursor
+                    x = (-x_offset + event.pos[0]) // size
+                    y = (-y_offset + event.pos[1]) // size
+                    if 0 < x < universe.shape[0] and 0 < y < universe.shape[1]:
+                        universe[x, y] = not universe[x, y]
+                mouse_down = False
+                mouse_move = False
         if event.type == pygame.MOUSEMOTION:
             if mouse_down:
                 mouse_move = True
@@ -76,6 +91,12 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 paused = not paused
+            if event.key == pygame.K_r:
+                universe = np.zeros_like(universe)
+            if event.key in (pygame.K_EQUALS, pygame.K_KP_PLUS):
+                x_offset, y_offset, size = zoom(True, screen_dims[0] // 2, screen_dims[1] // 2)
+            if event.key in (pygame.K_MINUS, pygame.K_KP_MINUS):
+                x_offset, y_offset, size = zoom(False, screen_dims[0] // 2, screen_dims[1] // 2)
 
     # clear image
     screen.fill("black")
